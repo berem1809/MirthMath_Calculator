05526f48ff9bc0429cc18377cf70da67959f1dbd
Piranaavei Beremkumar <108848056+berem1809@users.noreply.github.com>
2024-09-28 10:22:20 +0530

    Code for MirthMath_Calculator.

---
+++ src/DigitButton.jsx
@@ -0,0 +1,7 @@
+//DigitButton.js File
+
+import { ACTIONS } from './App';
+
+export default function DigitButton({ dispatch, digit }) {
+  return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>{digit}</button>;
+}
